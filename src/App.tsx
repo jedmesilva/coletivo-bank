c4a5fe34e75319da6fddc03e058f0c39210f5264
uncertainbobcat <42695231-uncertainbobcat@users.noreply.replit.com>
2025-05-15 00:08:07 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,40 @@
+
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AppProvider } from "@/context/AppContext";
+import DepositModal from "./components/DepositModal";
+import FundCreationModal from "./components/FundCreationModal_new";
+import CapitalRequestSheet from "./components/CapitalRequestSheet";
+import DebtPaymentSheet from "./components/DebtPaymentSheet";
+import Index from "./pages/Index";
+import NotFound from "./pages/NotFound";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <AppProvider>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+          {/* Global modals - available on any page */}
+          <DepositModal />
+          <FundCreationModal />
+          <CapitalRequestSheet />
+          <DebtPaymentSheet />
+        </BrowserRouter>
+      </AppProvider>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
