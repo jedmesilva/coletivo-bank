a62cade1e685909d96682ca08bf47bdec49d728d
Descart 31 Descart 31 <42937399-horribleotterli@users.noreply.replit.com>
2025-05-20 20:31:24 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter } from "react-router-dom";
+import { AppProvider } from "@/context/AppContext";
+import Index from "./pages/Index";
+import FundCreationModal from "./components/FundCreationModal";
+import DepositModal from "./components/DepositModal";
+import CapitalRequestSheet from "./components/CapitalRequestSheet";
+import DebtPaymentSheet from "./components/DebtPaymentSheet";
+import { Toaster } from "@/components/ui/sonner";
+
+
+const queryClient = new QueryClient();
+
+const App = () => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <AppProvider>
+        <BrowserRouter>
+          <div className="flex flex-col min-h-screen w-full overflow-x-hidden">
+            <main className="flex-1 w-full max-w-[1200px] mx-auto">
+              <Index />
+            </main>
+            <FundCreationModal />
+            <DepositModal />
+            <CapitalRequestSheet />
+            <DebtPaymentSheet />
+            <Toaster />
+          </div>
+        </BrowserRouter>
+      </AppProvider>
+    </QueryClientProvider>
+  );
+};
+
+export default App;
